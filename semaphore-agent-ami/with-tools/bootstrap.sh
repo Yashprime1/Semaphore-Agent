62798f733db092dcac427d56cb7475508382be27
yashdeep <yashdeep@clevertap.com>
2025-06-25 19:00:11 +0530

    updating agent ami

--- semaphore-agent-ami/with-tools/bootstrap.sh
+++ semaphore-agent-ami/with-tools/bootstrap.sh
@@ -2,13 +2,13 @@ echo "Installing semaphoreci tools"
 
 echo "Installing Semaphore CLI..."
 # Install Semaphore CLI
-curl -L https://github.com/semaphoreci/cli/releases/latest/download/sem_linux_amd64.tar.gz | tar -xz
+curl -L https://github.com/semaphoreci/cli/releases/download/v0.32.0/sem_Linux_x86_64.tar.gz | tar -xz
 sudo mv sem /usr/local/bin/
 sudo chmod +x /usr/local/bin/sem
 
 echo "Installing SPC (Semaphore Pipeline Compiler)..."
 # Install SPC
-curl -L https://github.com/semaphoreci/spc/releases/latest/download/spc_linux_amd64.tar.gz | tar -xz
+curl -L https://github.com/semaphoreci/spc/releases/download/v1.12.1/spc_Linux_x86_64.tar.gz | tar -xz
 sudo mv spc /usr/local/bin/
 sudo chmod +x /usr/local/bin/spc
 
