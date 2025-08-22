32a1361d408a3a1886a5bb955e3f6473b890c35e
yashdeep <yashdeep@clevertap.com>
2025-08-22 19:03:01 +0530

    semaphore: updating bootstrap scripts for ultron and tools and the ami
    SNE-50311

--- semaphore-agent-ami/with-tools/bootstrap.sh
+++ semaphore-agent-ami/with-tools/bootstrap.sh
@@ -32,7 +32,7 @@ chmod +x /usr/local/bin/spc
 echo "Installing Erlang..."
 apt-get -o DPkg::Lock::Timeout=300 install -f -y
 apt-get -o DPkg::Lock::Timeout=300 install -y --allow-change-held-packages linux-libc-dev
-apt-get -o DPkg::Lock::Timeout=300 install -y build-essential autoconf libtool
+apt-get -o DPkg::Lock::Timeout=300 install -y build-essential autoconf libtool libncurses5-dev
 curl -L https://github.com/erlang/otp/releases/download/OTP-26.1.2/otp_src_26.1.2.tar.gz | tar -xz
 cd otp_src_26.1.2
 ./configure --prefix=/opt/erlang
